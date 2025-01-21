a6b1bdd0d9e1310ebb3d42b2d61d7a00e2a60063
ye21iin <kathykr120@gmail.com>
2025-01-21 17:01:19 +0900

    Feat: create new HTML structure with form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h2>2024 파리 올림픽</h2>
+        <form className="form">
+          <div>
+            <h4>국가명</h4>
+            <input></input>
+          </div>
+          <div>
+            <h4>금메달</h4>
+            <input></input>
+          </div>
+          <div>
+            <h4>은메달</h4>
+            <input></input>
+          </div>
+          <div>
+            <h4>동메달</h4>
+            <input></input>
+          </div>
+          <div className="formBtns">
+            <button className="addBtn">국가 추가</button>
+            <button className="udBtn">업데이트</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
