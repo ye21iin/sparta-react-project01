85525889e4c165f8ad7a897f328cebfa7e09ab9e
ye21iin <kathykr120@gmail.com>
2025-01-22 20:51:05 +0900

    Refactor: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,7 @@ function App() {
         <Form records={records} setRecords={setRecords} />
       </div>
 
-      <div className="card">
-        {records.length === 0 ? (
-          <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
-        ) : (
-          records
-            .sort((a, b) => b.gold - a.gold) // 금메달 기준 내림차순 정렬
-            .map((r) => {
-              return (
-                <p key={r.id}>
-                  {r.country} {r.gold} {r.silver} {r.bronze}
-                </p>
-              );
-            })
-        )}
-      </div>
+      <List records={records} className="card"></List>
     </>
   );
 }
