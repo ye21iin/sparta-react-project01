1aaae537a84971a6961df6dd9cee86109a249447
ye21iin <kathykr120@gmail.com>
2025-01-22 17:20:30 +0900

    Feat: Add a list - showing current records - sorted by the number of gold medal Ref: CSS on progress

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,15 @@ function App() {
         {records.length === 0 ? (
           <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
         ) : (
-          <p>List</p>
+          records
+            .sort((a, b) => b.gold - a.gold) // 금메달 기준 내림차순 정렬
+            .map((r) => {
+              return (
+                <p key={r.id}>
+                  {r.country} {r.gold} {r.silver} {r.bronze}
+                </p>
+              );
+            })
         )}
       </div>
     </>
